7ad9b7e8b221af94aa797005c8a6d3fd99c4c30c
Durvesh More <durubhuru01@gmail.com>
2024-10-13 18:39:54 +0530

    Added: images & section 3rd complete

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-from datetime import time
+from datetime import datetime
+import pytz
 
-# Create a time object
-my_time = time(10, 30, 45)
+# Create a timezone-aware datetime object
+utc_zone = pytz.utc
+utc_datetime = datetime.now(utc_zone)
 
-# Format the time as "HH:MM:SS"
-formatted_time = my_time.strftime("%H:%M:%S")
-print("Formatted Time:", formatted_time)
+# Convert UTC time to a different time zone (e.g., US/Eastern)
+eastern_zone = pytz.timezone('US/Eastern')
+eastern_datetime = utc_datetime.astimezone(eastern_zone)
+
+print("UTC Datetime:", utc_datetime)
+print("Eastern Datetime:", eastern_datetime)
